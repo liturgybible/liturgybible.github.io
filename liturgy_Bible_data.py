36271a72be5adc216e0075cf578e7876ac87633e
Matthew Kudija <m.kudija@gmail.com>
2025-10-28 21:25:06 -0400

    spell out abbreviations

--- liturgy_Bible_data.py
+++ liturgy_Bible_data.py
@@ -16,7 +16,7 @@ COLOR_MAP = {
     "Sunday Reading": "#7B2E2B",
     "Solemnities & Feasts": "#C9A441",
     "Weekday Reading": "#B35C58",
-    "LOTH: OR": "#4C6B4F",
+    "LOTH: Office of Readings": "#4C6B4F",
     "LOTH: Psalms": "#5E4B81",
     "LOTH: Ordinary": "#5E4B81",
     "LOTH: Other": "#4A628A"
